c57bd5190eee8859772226be73ce5935e7ec4182
ioannides-agg <ioannides.agg@gmail.com>
2025-07-13 21:21:26 +0300

    removed commented out code

--- includes/camera.h
+++ includes/camera.h
@@ -52,45 +52,6 @@ public:
     }
 
     PPM_image_writer("render.ppm", width, height, framebuffer);
-
-    /*vec3f w = (camera_pos - look_at).normalized();
-    vec3f u = cross(vec3f(0.0f, 1.0f, 0.0f), w).normalized();
-    vec3f v = cross(w, u).normalized();
-
-    float viewport_height =
-        2.0f * tan(fov / 2.0f) *
-        -1.0f; // negative because the camera looks towards negative z
-    float viewport_width = 2.0f * tan(fov / 2.0f) * aspect_ratio;
-
-    point3f lower_left_corner = camera_pos - (viewport_width / 2.0f) * u -
-                                (viewport_height / 2.0f) * v - w;
-
-    for (size_t i = 0; i < height; i++) {
-      std::clog << "\rLines Remaining: " << height - i << " " << std::flush;
-      for (size_t j = 0; j < width; j++) {
-        color pixel_color(0.0f, 0.0f, 0.0f);
-        for (int sample = 0; sample < samples_per_pixel; sample++) {
-          float u_offset = (j + random_float() - 0.5f) / float(width);
-          float v_offset = (i + random_float() - 0.5f) / float(height);
-
-          vec3f direction = lower_left_corner + u_offset * viewport_width * u +
-                            v_offset * viewport_height * v - camera_pos;
-
-          Ray ray = Ray(camera_pos, direction.normalized());
-          pixel_color += color_ray(ray, world, max_depth);
-        }
-
-        interval in(0.000f, 0.999f);
-        pixel_color = pixel_color / samples_per_pixel;
-        color pixel(in.clamp(linear_to_gamma(pixel_color[0])),
-                    in.clamp(linear_to_gamma(pixel_color[1])),
-                    in.clamp(linear_to_gamma(pixel_color[2])));
-
-        framebuffer.push_back(pixel);
-      }
-    }
-
-    PPM_image_writer("render.ppm", width, height, framebuffer);*/
   }
 
 private:
